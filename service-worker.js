f777a501dded404954510806e3de34b52cd19720
johanlidstrom74-sudo <johan.lidstrom74@gmail.com>
2025-11-07 08:33:51 +0100

    Add service worker for caching assets
    Implement a service worker for caching and offline support.

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'keto-pwa-simple-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        return resp;
+      }).catch(() => caches.match('./index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(request).then(cached => cached || fetch(request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        return resp;
+      }))
+    );
+  }
+});
